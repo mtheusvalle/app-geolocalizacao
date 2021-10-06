704c6389946f9e03d901cd1f6b88e81abf8920aa
Matheus Valle <matheusvalleguitar@gmail.com>
2021-10-06 17:43:50 -0300

    Iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [address, setAddress] = useState([]);
+
+  const handleAddress = (address) => {
+    setAddress(address);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://api.geoapify.com/v1/geocode/search", {
+        params: {
+          text: "São Paulo",
+          apiKey: process.env.REACT_APP_KEY_API,
+        },
+      })
+      .then((response) => {
+        setAddress(response.data.features);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [address]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{address}</p>
+      <button onClick={() => handleAddress("29375000")}>clique aqui</button>
+    </>
   );
 }
-
-export default App;
